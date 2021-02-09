00c70ccd6f309fbce6b6ad456fd974552b576fcf
kz <kz@kz.kz>
2021-02-09 18:20:34 +0100

    transations in src dir

--- src/i18n.js
+++ src/i18n.js
@@ -1,37 +1,52 @@
-import i18n from 'i18next';
-import Backend from 'i18next-xhr-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import Backend from "i18next-xhr-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
 
-const fallbackLng = ['en'];
-const availableLanguages = ['en', 'ko', 'chi'];
+import translateEN from "./translations/en/translation.json";
+import translateCHI from "./translations/chi/translation.json";
+import translateKO from "./translations/ko/translation.json";
 
+const fallbackLng = ["en"];
+const availableLanguages = ["en", "ko", "chi"];
+
+const res = {
+  en: {
+    translation: translateEN,
+  },
+  chi: {
+    translation: translateCHI,
+  },
+  ko: {
+    translation: translateKO,
+  },
+};
 
 const options = {
   // order and from where user language should be detected
-  order: [ 'navigator', 'htmlTag', 'path', 'subdomain'],
+  order: ["navigator", "htmlTag", "path", "subdomain"],
 
   // keys or params to lookup language from
-  lookupQuerystring: 'lng',
-  lookupCookie: 'i18next',
-  lookupLocalStorage: 'i18nextLng',
+  lookupQuerystring: "lng",
+  lookupCookie: "i18next",
+  lookupLocalStorage: "i18nextLng",
   lookupFromPathIndex: 0,
   lookupFromSubdomainIndex: 0,
 
   // cache user language on
-  caches: ['localStorage', 'cookie'],
-  excludeCacheFor: ['cimode'], // languages to not persist (cookie, localStorage)
+  caches: ["localStorage", "cookie"],
+  excludeCacheFor: ["cimode"], // languages to not persist (cookie, localStorage)
 
   // optional expire and domain for set cookie
   cookieMinutes: 10,
-  cookieDomain: 'myDomain',
+  cookieDomain: "myDomain",
 
   // optional htmlTag with lang attribute, the default is:
   htmlTag: document.documentElement,
 
   // only detect languages that are in the whitelist
-  checkWhitelist: true
-}
+  checkWhitelist: true,
+};
 
 i18n
   .use(Backend) // load translation using xhr -> see /public/locales. We will add locales in the next step
@@ -45,9 +60,10 @@ i18n
     debug: true,
     whitelist: availableLanguages,
     detection: options,
+    resources: res,
 
     interpolation: {
-      escapeValue: false
+      escapeValue: false,
     },
   });
 
